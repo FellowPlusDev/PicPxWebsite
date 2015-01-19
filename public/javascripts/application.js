c642fff99907780cfcb7fe19afa34f5d3a867648
greatghoul <greatghoul@gmail.com>
2015-01-19 16:09:34 +0800

    重写复制链接地址
    使用 angularjs 重写上传和复制链接地址。

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,33 +1,68 @@
-angular
-  .module('DeepPicApp', ['angularFileUpload'])
-  .controller('UploadCtrl', function($scope, FileUploader) {
-    var uploader = $scope.uploader = new FileUploader({
-      url: '/image/upload',
-      autoUpload: true,
-      alias: 'image',
-      filters: [{
-        name: 'imageFilter',  
-        fn: function(item, options) {
-          return /image\/.*/.test(item.type)  ;
-        }
-      }]
-    });
+var app = angular.module('DeepPicApp', ['angularFileUpload', 'ngClipboard']);
 
-    uploader.onAfterAddingFile = function(item) {
-        console.log(item);
-    };
+app.config(['ngClipProvider', function(ngClipProvider) {
+  ngClipProvider.setPath("/bower_components/zeroclipboard/dist/ZeroClipboard.swf");
+}]);
 
-    uploader.onProgressItem = function(fileItem, progress) {
-      console.info('onProgressItem', fileItem, progress);
-    };
-
-    uploader.onSuccessItem = function(fileItem, response, status, headers) {
-      fileItem.remoteUrl = response.message;
-      console.info('onSuccessItem', fileItem, response, status, headers);
-    };
-
-
-    $scope.clickUpload = function() {
-      angular.element('#file').trigger('click');
-    }
+app.controller('UploadCtrl', function($scope, FileUploader) {
+  var uploader = $scope.uploader = new FileUploader({
+    url: '/image/upload',
+    autoUpload: true,
+    alias: 'image',
+    filters: [{
+      name: 'imageFilter',  
+      fn: function(item, options) {
+        return /image\/.*/.test(item.type)  ;
+      }
+    }]
   });
+
+  uploader.onProgressItem = function(fileItem, progress) {
+    // console.info('onProgressItem', fileItem, progress);
+  };
+
+  uploader.onSuccessItem = function(fileItem, response, status, headers) {
+    fileItem.remoteUrl = response.message;
+  };
+
+  $scope.clickUpload = function() {
+    angular.element('#file').trigger('click');
+  }
+
+  $scope.toggleSelect = function(item) {
+    if (item.isSuccess) {
+      item.isSelected = !(item.isSelected);
+    }
+  }
+
+  $scope.selectOutput = function() {
+    angular.element('#output textarea').focus().select();
+  }
+
+  $scope.selectAll = function() {
+    _.each(uploader.queue, function(item) {
+      if (item.isSuccess) {
+        item.isSelected = true;
+      }
+    });
+  }
+
+  $scope.selectNone = function() {
+    _.each(uploader.queue, function(item) {
+      if (item.isSuccess) {
+        item.isSelected = false;
+      }
+    });
+  }
+
+  $scope.isOutputVisible = function() {
+    return _.any(uploader.queue, { isSelected: true });
+  }
+
+  $scope.output = function() {
+    return _.chain(uploader.queue)
+            .where({ isSelected: true })
+            .map('remoteUrl')
+            .value().join('\n');
+  }
+});
